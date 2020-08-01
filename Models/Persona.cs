ca90d1f2a1bd1ee540415ec0c37335e79d1fecdb
gibranjulyano11 <gibranjulyano11@gmail.com>
2020-07-31 22:08:36 -0500

    Este es el deber de Vintage

---
+++ Models/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace VintageStuff.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public List<Camiseta> Camisetas { get; set; } = new List<Camiseta>();
+        public List<Boxer> Boxeres { get; set; } = new List<Boxer>();
+        public List<Pantalon> Pantalones { get; set; } = new List<Pantalon>();
+        public List<Polin> Polines { get; set; } = new List<Polin>();
+        public List<Top> Topes { get; set; } = new List<Top>();
+    }
+}
